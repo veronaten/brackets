60afe41f433a1bdbf9289ecc86a82041afacdb2c
Veronika <verona.binko@gmail.com>
2021-03-16 01:06:49 +0300

    feat: complete task breackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let prev = '';
+  for (let replaced = str; replaced != prev; prev = replaced, replaced = replaced.replace('()', '[]', '{}', '||', ''))
+  return prev == '';
+
+
+  const arr = [];
+   for (let i = 0; i < bracketsConfig.length; i++) {
+      arr.push(...bracketsConfig[i]);
+   }
+  let res = arr.join('');
+
+  const newArr = [res];
+  newArr.forEach(item => {
+    const result = check(item);
+    return (item, result);
+  })
+  
 }
